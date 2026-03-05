a6d8e9d8beccfab9cee699b29f9d68b5ac6555e2
Yashgamer-x <yashdesai121212@gmail.com>
2026-03-04 20:28:59 -0500

    Addd all the necessary file

---
+++ CartCollab-IOS-26-Yash/Account/View/AccountView.swift
@@ -0,0 +1,18 @@
+//
+//  AccountView.swift
+//  CartCollab-IOS-26-Yash
+//
+//  Created by Desai, Yash T on 3/4/26.
+//
+
+import SwiftUI
+
+struct AccountView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AccountView()
+}
---
+++ CartCollab-IOS-26-Yash/Account/View/ForgotPasswordView.swift
@@ -0,0 +1,18 @@
+//
+//  ForgotPasswordView.swift
+//  CartCollab-IOS-26-Yash
+//
+//  Created by Desai, Yash T on 3/4/26.
+//
+
+import SwiftUI
+
+struct ForgotPasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ForgotPasswordView()
+}
