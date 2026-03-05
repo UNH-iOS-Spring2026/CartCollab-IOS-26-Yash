fdbcf43dab75bfb3a291992b230adf9ec8d68a95
Yashgamer-x <yashdesai121212@gmail.com>
2026-03-04 20:18:10 -0500

    Initial Commit

---
+++ CartCollab-IOS-26-Yash/CartCollab_IOS_26_YashApp.swift
@@ -0,0 +1,17 @@
+//
+//  CartCollab_IOS_26_YashApp.swift
+//  CartCollab-IOS-26-Yash
+//
+//  Created by Desai, Yash T on 3/4/26.
+//
+
+import SwiftUI
+
+@main
+struct CartCollab_IOS_26_YashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CartCollab-IOS-26-YashTests/CartCollab_IOS_26_YashTests.swift
@@ -0,0 +1,17 @@
+//
+//  CartCollab_IOS_26_YashTests.swift
+//  CartCollab-IOS-26-YashTests
+//
+//  Created by Desai, Yash T on 3/4/26.
+//
+
+import Testing
+@testable import CartCollab_IOS_26_Yash
+
+struct CartCollab_IOS_26_YashTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
