846fc8d7a9205eab2f86c3405276a841abd22e11
Yashgamer-x <yashdesai121212@gmail.com>
2026-03-18 19:42:36 -0400

    Created model

--- CartCollab-IOS-26-Yash/Cart/Model/CartModel.swift
+++ CartCollab-IOS-26-Yash/Cart/Model/CartModel.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+final class CardModel: ObservableObject {
+    let id: String
+    
+    @Published var cartName: String = ""
+    @Published var cartCreatedBy: String = ""
+    @Published var locationName: String = ""
+    @Published var cartImageId: String = "cart.circle"
+    @Published var shoppingDate: Date = Date()
+    
+    init(id: String) {
+        self.id = id
+    }
+    
+    convenience init?(id: String, data: [String : Any]) {
+        guard let cartName = data["cartName"] as? String,
+              let cartCreatedBy = data["cartCreatedBy"] as? String?,
+              let locationName = data["locationName"] as? String?,
+              let cartImageId = data["cartImageId"] as? String?,
+              let shoppingDateTimeStamp = data["shoppingDate"] as? Double?
+        else {
+            return nil
+        }
+        
+        self.init(id: id)
+        self.cartName = cartName
+        self.cartCreatedBy = cartCreatedBy ?? ""
+        self.locationName = locationName ?? ""
+        self.cartImageId = cartImageId ?? ""
+        self.shoppingDate = Date(timeIntervalSince1970: shoppingDateTimeStamp ?? 0)
+    }
+}
