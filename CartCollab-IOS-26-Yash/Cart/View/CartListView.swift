4af0dbde6bd7b08cf4f271435b389cb52df79dcc
Yashgamer-x <yashdesai121212@gmail.com>
2026-03-18 20:11:02 -0400

    List View Complete

--- CartCollab-IOS-26-Yash/Cart/View/CartListView.swift
+++ CartCollab-IOS-26-Yash/Cart/View/CartListView.swift
@@ -8,8 +8,23 @@
 import SwiftUI
 
 struct CartListView: View {
+    @State var carts: [CartModel] = [
+        CartModel(id: "1", data: ["cartName" : "Cart 1", "cartCreatedBy" : "Jane Doe", "cartImageId" : "cart.circle"])!,
+        CartModel(id: "2", data: ["cartName" : "Cart 2", "cartCreatedBy" : "John Doe", "cartImageId" : "cart.fill"])!,
+        CartModel(id: "3", data: ["cartName" : "Cart 3", "cartCreatedBy" : "Jora Doe", "cartImageId" : "cart.circle"])!,
+        CartModel(id: "4", data: ["cartName" : "Cart 4", "cartCreatedBy" : "Jora Doe", "cartImageId" : "cart.circle"])!,
+        CartModel(id: "5", data: ["cartName" : "Cart 5", "cartCreatedBy" : "Jarl Doe", "cartImageId" : "cart.circle"])!,
+    ]
+    
     var body: some View {
-        Text("Card List View")
+        var list = ScrollView {
+            ForEach(carts, id: \.self.id) {
+                (cart: CartModel) in
+                CartViewModel(cart: cart)
+            }
+        }
+        
+        list
     }
 }
 
